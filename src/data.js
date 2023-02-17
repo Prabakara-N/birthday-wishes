edde12abf1ca4cf5eb1d1efac1b9de68eec5b50f
prabakaran <mspk028@gmail.com>
2023-02-17 14:58:26 +0530

    usestate added

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    img: "https://www.teahub.io/photos/full/4-47096_cat-wallpaper-background-data-src-free-download-cat.jpg",
+    name: "Kitty",
+    age: 3,
+  },
+  {
+    id: 2,
+    img: "https://wallpaperaccess.com/full/1209275.jpg",
+    name: "cat",
+    age: 6,
+  },
+  {
+    id: 3,
+    img: "https://w0.peakpx.com/wallpaper/234/475/HD-wallpaper-cat-black-cats-eyes-kitten-butterfly.jpg",
+    name: "blacky",
+    age: 8,
+  },
+  {
+    id: 4,
+    img: "https://www.imagesjunction.com/images/img/cat_images.jpg",
+    name: "white",
+    age: 7,
+  },
+  {
+    id: 5,
+    img: "https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2NDg2Mjc2LmFwcDQ0NzA4NV9zY3JlZW5zaG90c18wXzQzMzg3ZDBk/screen-0.jpg?fakeurl=1&type=.webp",
+    name: "snow",
+    age: 5,
+  },
+];
+
+export default data;
